78f8dad45737d43cb9a11276371b71b44d92d3d7
williambl <william@williambl.com>
2019-09-08 18:21:55 +0100

    Make server spawn particle when it receives ShowThirdPersonGuiMessage

--- src/main/java/com/williambl/thirdpersongui/common/networking/ShowThirdPersonGuiMessage.java
+++ src/main/java/com/williambl/thirdpersongui/common/networking/ShowThirdPersonGuiMessage.java
@@ -1,9 +1,13 @@
 package com.williambl.thirdpersongui.common.networking;
 
 import net.minecraft.network.PacketBuffer;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.network.NetworkEvent;
 
+import java.util.Objects;
 import java.util.function.Supplier;
 
 public class ShowThirdPersonGuiMessage {
@@ -23,6 +27,23 @@ public class ShowThirdPersonGuiMessage {
     }
 
     public void handle(Supplier<NetworkEvent.Context> context) {
-        System.out.println("recieved!");
+        if (context.get().getDirection().getReceptionSide() != LogicalSide.SERVER)
+            return;
+        context.get().enqueueWork(
+                () -> {
+                    ((ServerWorld)(Objects.requireNonNull(context.get().getSender()).world)).spawnParticle(
+                            ParticleTypes.BARRIER,
+                            this.pos.getX(),
+                            this.pos.getY(),
+                            this.pos.getZ(),
+                            1,
+                            0,
+                            0,
+                            0,
+                            0
+                    );
+                }
+        );
+        context.get().setPacketHandled(true);
     }
 }
